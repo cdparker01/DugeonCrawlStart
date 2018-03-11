e7a7dc0444e29ef9754f18b04d1a4d59917a62da
Chandler Parker <chandlerparker@Chandlers-MacBook-Air.local>
2018-03-10 19:23:27 -0500

    Added client server test

--- Client.java
+++ Client.java
@@ -1,5 +1,37 @@
-public class Client
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DungeonCrawlStart;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author chandlerparker
+ */
+public class Client 
 {
-
-
+    public static void main(String[] args) throws UnknownHostException, IOException
+    {
+        int number,temp;
+        Scanner scan = new Scanner(System.in);
+        Socket s = new Socket("2601:543:102:89d9:1de:fd56:4018:cb1a", 3000);
+        
+        Scanner scan2 = new Scanner(s.getInputStream());
+        System.out.println("Enter any number:");
+        number = scan.nextInt();
+        
+        PrintStream p = new PrintStream(s.getOutputStream());
+        p.println(number);
+        temp = scan2.nextInt();
+        System.out.println(temp);
+    }
 }
--- Server.java
+++ Server.java
@@ -1,4 +1,36 @@
-public class Server
-{
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DungeonCrawlStart;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author chandlerparker
+ */
+public class Server 
+{
+    public static void main(String[] args) throws IOException
+    {
+        int number,temp;
+        ServerSocket ser = new ServerSocket(3000);
+        Socket ss = ser.accept();
+        System.out.println("Connected");
+        
+        Scanner scan = new Scanner(ss.getInputStream());
+        number = scan.nextInt();
+        
+        temp = number + 2; //Manipulated input
+        
+        PrintStream ps = new PrintStream(ss.getOutputStream());
+        ps.println(temp); // printed manipulated output
+        
+    }
 }
